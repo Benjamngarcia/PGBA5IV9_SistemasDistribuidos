7084bd304c940d158242c0861492763ac304da35
Benjamín Arturo Pérez García <bperezg1800@alumno.ipn.mx>
2021-09-06 23:12:41 -0500

    aaaaaa

--- Serialización/InvertirCadena/src/invertircadena/Funciones.java
+++ Serialización/InvertirCadena/src/invertircadena/Funciones.java
@@ -1,5 +1,14 @@
-package ejercicio.pkg1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invertircadena;
 
+/**
+ *
+ * @author bengy
+ */
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
